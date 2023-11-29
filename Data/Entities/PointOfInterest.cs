274bf04e0398d04d4e941324e51083d54c174264
Jorge Urioste <a2021110231@estudiantes.upsa.edu.bo>
2023-11-29 08:34:48 -0400

    feat: Adding a Column to pointsOfInterest and seeding the database

--- Data/Entities/PointOfInterest.cs
+++ Data/Entities/PointOfInterest.cs
@@ -8,9 +8,13 @@ public class PointOfInterest
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id {get; set;}
+    
     [Required]
     [MaxLength(50)]
     public string Name {get; set;}
+    
+    [MaxLength(200)]
+    public string? Description {get; set;}
     
     [ForeignKey("CityId")]
     public City? City { get; set; }
--- Migrations/20231128232311_DataSeed.Designer.cs
+++ Migrations/20231128232311_DataSeed.Designer.cs
@@ -2,6 +2,7 @@
 using CityInfo.API.Dbcontext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CityInfo.API.Migrations
 {
     [DbContext(typeof(CityInfoContext))]
-    partial class CityInfoContextModelSnapshot : ModelSnapshot
+    [Migration("20231128232311_DataSeed")]
+    partial class DataSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.0");
